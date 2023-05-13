82f2707cb579e0b24fba4170b8858a68e39edbdc
Wppopqpu <wppopqpu@outlook.com>
2023-05-13 12:10:56 +0800

    add xmake.lua

--- xmake.lua
+++ xmake.lua
@@ -1,1 +1,15 @@
+set_rules('mode.debug', 'mode.release')
+set_languages('c++20')
 
+
+target('omniMatrices')
+	set_kind('static')
+	set_policy('build.c++.modules', true)
+	add_files('src/omniMatrices/*.cpp|src/omniMatrices/*.m.cpp')
+	add_files('src/library/*.m.cpp', { install = true })
+
+target('test')
+	set_kind('binary')
+	set_policy('build.c++.modules', true)
+	add_files('src/main.cpp')
+	add_deps('omniMatrices')
