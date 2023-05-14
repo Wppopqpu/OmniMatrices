6c05d7f89d33808d3b7f2a352a58efb2a3813e50
Wppopqpu <wppopqpu@outlook.com>
2023-05-14 10:00:38 +0800

    fix some mistakes

--- src/omniMatrices/Matrices.cpp
+++
@@ -1,8 +0,0 @@
-module OmniMatrices.Matrices;
-
-namespace OmniMatrices
-{
-	inline namespace Matrices
-	{
-	}
-}
---
+++ src/omniMatrices/matrices.cpp
@@ -0,0 +1,8 @@
+module OmniMatrices.Matrices;
+
+namespace OmniMatrices
+{
+	inline namespace Matrices
+	{
+	}
+}
